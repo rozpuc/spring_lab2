501faa6d1072ef81124cf835beaddb8ab68dadea
rozpuc <mleczko.wik@gmail.com>
2026-03-28 12:32:19 +0100

    Implement HelloController with page routes
    Add HelloController with public and secured page mappings

--- src/main/java/pl/com/pl/lab2/HelloController.java
+++ src/main/java/pl/com/pl/lab2/HelloController.java
@@ -1,1 +1,26 @@
+package pl.com.pl.lab2;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    public String publicPage() {
+        return "index";
+    }
+
+    @GetMapping("/secured")
+    public String securedPage(Authentication authentication, Model model) {
+        if (authentication.getName().equals("admin")) {
+            model.addAttribute("message", "Zalogowano administratora");
+        } else {
+            model.addAttribute("message", "Numer indeksu: 12345");
+        }
+        return "secured";
+    }
+}
 
