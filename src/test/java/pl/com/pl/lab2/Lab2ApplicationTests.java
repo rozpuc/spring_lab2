32dc972f9064423022aa29ec651de203f67318ad
rozpuc <mleczko.wik@gmail.com>
2026-03-28 12:36:09 +0100

    Add initial test class for Lab2Application

---
+++ src/test/java/pl/com/pl/lab2/Lab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.com.pl.lab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritydemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
